f39833f03f3c7c7477c93267535792f42b73a815
Arne Woerheide <arnewo@hotmail.com>
2025-05-08 11:56:47 +0000

    header added

---
+++ INSTRUMENTS/include/InstrumentsImpl.h
@@ -0,0 +1,27 @@
+#ifndef _INSTRUMENT_COMPONENT_IMPL_H
+#define _INSTRUMENT_COMPONENT_IMPL_H
+ 
+#ifndef __cplusplus
+#error This is a C++ include file and cannot be used from plain C
+#endif
+ 
+//Base component implementation, including container services and component lifecycle infrastructure
+#include <acscomponentImpl.h>
+ 
+//Skeleton interface for server implementation
+#include <HelloComponentS.h>
+ 
+//Error definitions for catching and raising exceptions
+class InstrumentComponentImpl : public virtual acscomponent::ACSComponentImpl, public virtual POA_workshop::InstrumentComponent {
+  public:
+    InstrumentComponentImpl(const ACE_CString& name, maci::ContainerServices * containerServices);
+    virtual ~InstrumentComponentImpl();
+    void cameraOn ();
+	void cameraOff ();
+	TYPES::ImageType takeImage(long exposureTime);
+	void setRGB(TYPES::RGB rgbConfig);
+	void setPixelBias( long bias);
+	void setResetLevel(long resetLevel);
+};
+ 
+#endif
